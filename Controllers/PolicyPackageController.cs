96fd556a6ce3e799b46d5e9c9ae22290340f8382
chatapazar <31641737+chatapazar@users.noreply.github.com>
2020-08-20 10:31:05 +0700

    init

---
+++ Controllers/PolicyPackageController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PolicyPackageController : ControllerBase
+    {
+        // GET api/policypackages
+        [HttpGet]
+        public ActionResult<IEnumerable<PolicyPackage>> Get()
+        {
+            PolicyPackage[] C = new PolicyPackage[3];
+            C[0] = new PolicyPackage { PackageName  = "Silver Package", PackageNo = "2020080001"};
+			      C[1] = new PolicyPackage { PackageName  = "Gold Package", PackageNo = "2020080002"};
+            C[2] = new PolicyPackage { PackageName  = "Platinum Package", PackageNo = "2020080003"};
+            
+			return C;
+        }
+
+        // GET api/policypackages/5
+        [HttpGet("{id}")]
+        public ActionResult<PolicyPackage> Get(int id)
+        {
+            C[0] = new PolicyPackage { PackageName  = "Silver Package", PackageNo = "2020080001"};
+        }
+
+    }
+}
