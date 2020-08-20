18febce4782d3c0002395f03db2b3cad18447802
chatapazar <31641737+chatapazar@users.noreply.github.com>
2020-08-20 16:53:40 +0700

    Delete Customer.cs

--- Controllers/Customer.cs
+++
@@ -1,9 +0,0 @@
-namespace Test.Models
-{
-    public class Customer
-    {
-        public string CustName { get; set; }
-        public string CustNo { get; set; }
-        public string PolicyNo { get; set; }
-    }
-}
